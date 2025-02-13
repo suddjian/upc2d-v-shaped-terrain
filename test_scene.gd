55ae246f5166b27081598d6a200735d269ba2fb2
David Aaron Suddjian <aasuddjian@gmail.com>
2025-02-12 17:24:04 -0800

    test scene changes

--- test_scene.gd
+++ test_scene.gd
@@ -2,23 +2,19 @@ extends Node
 
 var character_scene = preload("res://character_body_2d.tscn")
 var patched_script = preload("res://addons/Ultimate-Platformer-Controller-2D/UltimatePlatformerController.gd")
+var unpatched_script = preload("res://UltimatePlatformerController_prepatch.gd")
 
-var character: CharacterBody2D = null
+@export var character: CharacterBody2D
 
-func spawn_character(script=null):
-	if character:
-		character.queue_free()
-	character = character_scene.instantiate()
-	if script:
-		character.set_script(script)
-		character.PlayerSprite = character.get_node("Sprite")
-		character.PlayerCollider = character.get_node("Collider")
+
+func _on_spawn_pressed() -> void:
 	character.global_position = %SpawnPoint.global_position
 	character.reset_physics_interpolation()
-	add_child(character)
+	
 
-func _on_use_unpatched_controller_pressed() -> void:
-	spawn_character()
-
-func _on_use_patched_controller_pressed() -> void:
-	spawn_character(patched_script)
+func _on_use_patch_toggled(toggled_on: bool) -> void:
+	var script = patched_script if toggled_on else unpatched_script
+	character.set_script(script)
+	character.PlayerSprite = character.get_node("Sprite")
+	character.PlayerCollider = character.get_node("Collider")
+	character._ready()
